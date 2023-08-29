00d4f4684e307f1487cbdb42ec90bc1a3676c7bc
FERNANDAFREITAS29 <fer.s.freitas29@gmail.com>
2023-08-29 19:55:35 -0300

    first commit

---
+++ .vscode/desafio1608.py
@@ -0,0 +1,42 @@
+
+#desafio16
+'''
+num = 10
+
+if num >= 10 or num > 20:
+    print('o numero é maior que 10')
+else:
+    print('o numero é menor ou igual a 10')
+
+num = 9
+
+if num >= 10 or num > 20:
+    print('o numero é maior que 10')
+else:
+    print('o numero é menor ou igual a 10')
+
+'''
+'''
+#desafio17
+
+idade = 29
+
+if idade <= 13 or idade < 29:
+    print('VOCÊ É UMA CRIANÇA')
+if idade <= 13 or idade < 19:
+    print('VOCÊ É UM ADOLESCENTE')
+else:
+    print('VOCÊ É UM ADULTO')
+    '''
+
+#desafio18
+
+lista = ['BMW X6', 'BMW I5', 'BMW I8']
+
+for Carro in range(3):
+ 
+ print(lista[Carro])
+
+
+
+
